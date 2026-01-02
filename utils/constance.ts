0effffaca6629e7e59e02efa30abe5fc947d5f1d
codewikAasmini <aasmini.codewik@gmail.com>
2026-01-02 18:40:56 +0530

    contance change for live

--- utils/constance.ts
+++ utils/constance.ts
@@ -2,5 +2,11 @@
 // export const APIBaseUrl = "http://localhost:4999/api/";
 // export const ImageBaseUrl = "http://localhost:4999/";
 
-export const APIBaseUrl = "http://192.168.1.11:4999/api/";
-export const ImageBaseUrl = "http://192.168.1.11:4999/";
+// export const APIBaseUrl = "http://192.168.1.11:4999/api/";
+// export const ImageBaseUrl = "http://192.168.1.11:4999/";
+
+export const APIBaseUrl = "http://13.62.243.93:4999/api/";
+
+export const ImageBaseUrl = "http://13.62.243.93:4999/";
+
+ 
